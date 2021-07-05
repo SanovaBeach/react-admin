fdbf0bafb22ae5ede495e627a94bfcda4a175a3f
Laurence <laurencebalazo@gmail.com>
2021-07-05 13:41:11 +0800

    downloaded packages

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const overallSalesMonthly = [
+  {
+    month : 'Jan',
+    sales: 20000,
+  },
+  {
+    month : 'Feb',
+    sales: 25500,
+  },
+  {
+    month : 'Mar',
+    sales: 21000,
+  },
+  {
+    month : 'Apr',
+    sales: 30000,
+  },
+  {
+    month : 'May',
+    sales: 40000,
+  },
+  {
+    month : 'Jun',
+    sales: 37499,
+  },
+  {
+    month : 'Jul',
+    sales: 29999,
+  },
+  {
+    month : 'Aug',
+    sales: 25000,
+  },
+  {
+    month : 'Sep',
+    sales: 15000,
+  },
+  {
+    month : 'Oct',
+    sales: 22000,
+  },
+  {
+    month : 'Nov',
+    sales: 23000,
+  },
+  {
+    month : 'Dec',
+    sales: 30000,
+  },
+]
